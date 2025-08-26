16bfa03ced21d8b6ae7b48020bb75d8cd21ac05a
Klimchuk Daniil <klimchuk.daniil@gmail.com>
2025-08-26 08:07:12 +0300

    Use version catalog from build logic

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -16,7 +16,7 @@ dependencyResolutionManagement {
         google()
         mavenCentral()
     }
-    versionCatalogs.create("libs").from(files("../libs.versions.toml"))
+    versionCatalogs.create("libs").from(files("../build-logic/libs.versions.toml"))
 }
 
 rootProject.name = "Elmslie"
